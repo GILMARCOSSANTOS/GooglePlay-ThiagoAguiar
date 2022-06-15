4d513adbcd2af2babeaf66c3f013ef694f578843
Gilmarcos Santos <GILMARCOSXSANTOS@GMAIL.COM>
2022-06-14 23:44:55 -0300

    Salvar Projeto 14/06/2022

--- app/src/main/java/com/example/googleplay_thiagoaguiar10/model/GameType.kt
+++ app/src/main/java/com/example/googleplay_thiagoaguiar10/model/GameType.kt
@@ -2,6 +2,17 @@ package com.example.googleplay_thiagoaguiar10.model
 
 enum class GameType (val label: String) {
 
+    /*
+     <!--
+   ▬▬▬ DOCUMENTAÇÃO = Aula 03 = 001:
+   ▬ Navegação entre Activitys:
+   • Activity Anterior: XXXXX;
+   • Activity Atual: GameType;
+   • Activity Próxima: fragment_game.xml;
+   ▬ Descrição:
+   • O GameType Foi recortado de MainFragment;
+   -->
+     */
 
     FOR_YOU("For you"),
     TOP_CHARTS("Top charts"),
